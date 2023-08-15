ea6b802124e8333720444118742f7d9e55963d6a
MollyXuemn <Molly504954366@gmail.com>
2023-08-15 15:43:10 +0200

    add security dependency

--- src/main/java/com/meini/springsecuritybasic/SpringsecuritybasicApplication.java
+++ src/main/java/com/meini/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,11 +2,13 @@ package com.meini.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringsecuritybasicApplication.class, args);
 	}
 
