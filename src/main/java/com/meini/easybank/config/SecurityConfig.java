d79b9c97cf1336336fd054fd237c33f15fbf409d
MollyXuemn <Molly504954366@gmail.com>
2023-08-17 16:59:57 +0200

    add Spring RESTApi login and our own DB customer

---
+++ src/main/java/com/meini/easybank/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.meini.easybank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                .requestMatchers("/contact","notices","register").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsManager(){*/
+       /* Approach 1 where we use withDefaultPasswordEncoder() method
+        while creating the user details*/
+    /*    UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }*/
+   /* @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }*/
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/com/meini/easybank/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.meini.easybank.repository;
+
+import com.meini.easybank.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByEmail(String email);
+
+}
