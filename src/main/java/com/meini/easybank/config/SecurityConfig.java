e7153854a54ee9fb32a875702dbfa4c0a88e98af
MollyXuemn <Molly504954366@gmail.com>
2023-08-18 11:01:26 +0200

    password security:  BCryptPasswordEncoder

--- src/main/java/com/meini/easybank/config/SecurityConfig.java
+++ src/main/java/com/meini/easybank/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -26,41 +27,9 @@ public class SecurityConfig {
                 .httpBasic();
         return http.build();
     }
-    /*@Bean
-    public InMemoryUserDetailsManager userDetailsManager(){*/
-       /* Approach 1 where we use withDefaultPasswordEncoder() method
-        while creating the user details*/
-    /*    UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin,user);
-    }*/
-   /* @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin,user);
-    }*/
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource){
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/meini/easybank/controller/LoginController.java
+++ src/main/java/com/meini/easybank/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.meini.easybank.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
     CustomerRepository customerRepository;
+    PasswordEncoder passwordEncoder;
 
     @Autowired
-    public LoginController(CustomerRepository customerRepository) {
+    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer){
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
             if(savedCustomer.getId() > 0){
                 response = ResponseEntity
